0d272b5f9ac043d359eb7508c02ee64f51b0929d
Azraelki <584917180@qq.com>
2018-12-02 14:36:50 +0800

    chapter-7 sklearn演示AdaBoost算法

--- machinelearn/chapter7/app.py
+++ machinelearn/chapter7/app.py
@@ -174,14 +174,14 @@ def function4():
 
     tree = DecisionTreeClassifier(criterion='entropy',
                                   random_state=1,
-                                  max_depth=1)
+                                  max_depth=None)
     bag = BaggingClassifier(base_estimator=tree,
-                            n_estimators=1000,
+                            n_estimators=500,
                             max_samples=1.0,
                             max_features=1.0,
                             bootstrap=True,
                             bootstrap_features=False,
-                            n_jobs=1,
+                            n_jobs=-1,
                             random_state=1)
 
     # 使用单颗决策树
@@ -193,6 +193,7 @@ def function4():
     tree_test = accuracy_score(y_test, y_test_pred)
     print('Decision tree train/test accuracies %.3f/%.3f'% (tree_train, tree_test))
 
+    # 使用bagging集成分类器
     bag = bag.fit(X_train, y_train)
     y_train_pred = bag.predict(X_train)
     y_test_pred = bag.predict(X_test)
@@ -200,16 +201,112 @@ def function4():
     bag_test = accuracy_score(y_test, y_test_pred)
     print('Bagging train/test accuracies %.3f/%.3f'% (bag_train, bag_test))
 
+    # 绘制两个模型的决策区域
+    x1_min = X_train[:,0].min()-1
+    x1_max = X_train[:,0].max()+1
+    x2_min = X_train[:,1].min()-1
+    x2_max = X_train[:,1].max()+1
 
+    xx1,xx2 = np.meshgrid(np.arange(x1_min,x1_max,0.1),np.arange(x2_min,x2_max))
 
+    f,axarr = plt.subplots(nrows=1,ncols=2,sharex='col',sharey='row',figsize=(8,3))
 
+    for idx,clf,tt in zip([0,1],[tree,bag],['decision tree','bagging']):
+        clf.fit(X_train,y_train)
+        Z = clf.predict(np.c_[xx1.ravel(),xx2.ravel()])
+        Z = Z.reshape(xx1.shape)
+        axarr[idx].contourf(xx1,xx2,Z,alpha=0.3)
+        axarr[idx].scatter(X_train[y_train==0,0],X_train[y_train==0,1],
+                           c='blue',marker='^')
+        axarr[idx].scatter(X_train[y_train==1,0],X_train[y_train==1,1],
+                           c='green',marker='o')
+        axarr[idx].set_title(tt)
+    axarr[0].set_ylabel('alcohol',fontsize=12)
+    plt.text(10.2,-1.2,s='OD280/OD315 of diluted wines',
+             ha='center',va='center',fontsize=12)
+    plt.show()
 
+############################AdaBoost算法###############################
+#
+def function5():
+    from sklearn.ensemble import AdaBoostClassifier
+    df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/'
+                          'machine-learning-databases/wine/wine.data',
+                          header=None)
+    df_wine.columns = ['Class label', 'Alcohol',
+                       'Malic acid', 'Ash',
+                       'Alcalinity of ash',
+                       'Magnesium', 'Total phenols',
+                       'Flavanoids', 'Nonflavanoid phenols',
+                       'Proanthocyanins',
+                       'Color intensity', 'Hue',
+                       'OD280/OD315 of diluted wines',
+                       'Proline']
+    # drop 1 class
+    df_wine = df_wine[df_wine['Class label'] != 1]
+    y = df_wine['Class label'].values
+    X = df_wine[['Alcohol', 'OD280/OD315 of diluted wines']].values
+    le = LabelEncoder()
+    y = le.fit_transform(y)
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y,
+                         test_size=0.2,
+                         random_state=1,
+                         stratify=y)
 
+    tree = DecisionTreeClassifier(criterion='entropy',
+                                  random_state=1,
+                                  max_depth=1)
+    ada = AdaBoostClassifier(base_estimator=tree,
+                             n_estimators=500,
+                             learning_rate=0.1,
+                             random_state=1)
 
+    # 单颗决策树(弱学习机)
+    tree = tree.fit(X_train,y_train)
+    y_train_pred = tree.predict(X_train)
+    y_test_pred = tree.predict(X_test)
+    tree_train = accuracy_score(y_train, y_train_pred)
+    tree_test = accuracy_score(y_test, y_test_pred)
+    print('Decision tree train/test accuracies %.3f/%.3f' % (tree_train, tree_test))
+
+    # AdaBoost算法
+    ada = ada.fit(X_train, y_train)
+    y_train_pred = ada.predict(X_train)
+    y_test_pred = ada.predict(X_test)
+    tree_train = accuracy_score(y_train, y_train_pred)
+    tree_test = accuracy_score(y_test, y_test_pred)
+    print('AdaBoost train/test accuracies %.3f/%.3f' % (tree_train, tree_test))
+
+    # 绘制两个模型的决策区域
+    x1_min = X_train[:, 0].min() - 1
+    x1_max = X_train[:, 0].max() + 1
+    x2_min = X_train[:, 1].min() - 1
+    x2_max = X_train[:, 1].max() + 1
+
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, 0.1), np.arange(x2_min, x2_max))
+
+    f, axarr = plt.subplots(nrows=1, ncols=2, sharex='col', sharey='row', figsize=(8, 3))
+
+    for idx, clf, tt in zip([0, 1], [tree, ada], ['decision tree', 'bagging']):
+        clf.fit(X_train, y_train)
+        Z = clf.predict(np.c_[xx1.ravel(), xx2.ravel()])
+        Z = Z.reshape(xx1.shape)
+        axarr[idx].contourf(xx1, xx2, Z, alpha=0.3)
+        axarr[idx].scatter(X_train[y_train == 0, 0], X_train[y_train == 0, 1],
+                           c='blue', marker='^')
+        axarr[idx].scatter(X_train[y_train == 1, 0], X_train[y_train == 1, 1],
+                           c='green', marker='o')
+        axarr[idx].set_title(tt)
+    axarr[0].set_ylabel('alcohol', fontsize=12)
+    plt.text(10.2, -1.2, s='OD280/OD315 of diluted wines',
+             ha='center', va='center', fontsize=12)
+    plt.show()
 
 
 if __name__ == "__main__":
     # function1()
     # function2()
     # function3()
-    function4()
+    # function4()
+    function5()
