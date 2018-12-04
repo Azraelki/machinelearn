ac076537c70dbe8d34fd842ee654067922127b52
azrael <584917180@qq.com>
2018-12-04 09:57:37 +0800

    chapter-9 sqlite建表，为之后建立web做准备，此处不再实现web端

--- machinelearn/chapter9/app.py
+++ machinelearn/chapter9/app.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import pickle
+import sqlite3
 from machinelearn.chapter8.app import function4,stop
 from movieclassifier.vectorizer import vect
 # 序列化chapter-8中的外存学习模型
@@ -22,7 +23,29 @@ def function2():
                                                 np.max(clf.predict_proba(X)*100)))
 
 
+# sqlite建表和测试
+def function3():
+    reviews = os.path.join("movieclassifier","reviews.sqlite")
+    if os.path.exists(reviews):
+        os.remove(reviews)
+    conn = sqlite3.connect(reviews)
+    c = conn.cursor()
+    c.execute("CREATE TABLE review_db(review Text,sentiment INTEGER, date Text)")
+
+    example1 = 'I love this movie'
+    c.execute("INSERT INTO  review_db(review,sentiment,date) VALUES(?,?,DATETIME('NOW') )",
+              (example1,1))
+
+    example2 = 'I disliked this movie'
+    c.execute("INSERT INTO  review_db(review,sentiment,date) VALUES(?,?,DATETIME('NOW') )",
+              (example2, 0))
+
+    conn.commit()
+    conn.close()
+
+
 
 if __name__ == '__main__':
     # function1()
-    function2()
+    # function2()
+    function3()
